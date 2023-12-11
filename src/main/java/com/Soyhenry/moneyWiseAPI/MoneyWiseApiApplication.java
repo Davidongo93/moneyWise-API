6419dfe3c4a5ad065968c66c2bad88c48ec11516
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-12-11 17:45:19 -0500

    First commit

---
+++ src/main/java/com/Soyhenry/moneyWiseAPI/MoneyWiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.Soyhenry.moneyWiseAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyWiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyWiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Soyhenry/moneyWiseAPI/MoneyWiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Soyhenry.moneyWiseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyWiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
